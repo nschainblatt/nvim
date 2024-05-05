a4f88be82bc539f84d96e351b21ae48435e37def
nschainblatt <nate@bluearrowmail.com>
2024-05-05 14:48:58 -0400

    Configuring mason

--- lua/nate/packer.lua
+++ lua/nate/packer.lua
@@ -4,45 +4,64 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.6',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use({
-		"folke/tokyonight.nvim",
-		config = function()
-			vim.cmd('colorscheme tokyonight-storm')
-		end	
-	})
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use('nvim-treesitter/playground')
-	use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
-	use {
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		requires = { {"nvim-lua/plenary.nvim"} }
-	}
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
-	use('lewis6991/gitsigns.nvim')
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		requires = {
-			--- Uncomment the two plugins below if you want to manage the language servers from neovim
-			-- {'williamboman/mason.nvim'},
-			-- {'williamboman/mason-lspconfig.nvim'},
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
 
-			{'neovim/nvim-lspconfig'},
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	}
+  -- Theme
+  use({
+    "folke/tokyonight.nvim",
+    config = function()
+      vim.cmd('colorscheme tokyonight-storm')
+    end	
+  })
+
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+  use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
+
+  use {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { {"nvim-lua/plenary.nvim"} }
+  }
+
+  use('mbbill/undotree')
+
+  use('tpope/vim-fugitive')
+
+  use('lewis6991/gitsigns.nvim')
+
+--  use {
+--    'VonHeikemen/lsp-zero.nvim',
+--    branch = 'v3.x',
+--    requires = {
+--      --- Uncomment the two plugins below if you want to manage the language servers from neovim
+--      -- {'williamboman/mason.nvim'},
+--      -- {'williamboman/mason-lspconfig.nvim'},
+--
+--      {'neovim/nvim-lspconfig'},
+--      {'hrsh7th/nvim-cmp'},
+--      {'hrsh7th/cmp-nvim-lsp'},
+--      {'L3MON4D3/LuaSnip'},
+--    }
+--  }
+
   use('ThePrimeagen/vim-be-good')
+
   use('m4xshen/hardtime.nvim')
+
+  use{
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'nvim/nvim-dap',
+    requires = {
+      'neovim/nvim-lspconfig'
+    }
+  }
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,16 +74,6 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
-  LuaSnip = {
-    loaded = true,
-    path = "C:\\Users\\Nate\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\LuaSnip",
-    url = "https://github.com/L3MON4D3/LuaSnip"
-  },
-  ["cmp-nvim-lsp"] = {
-    loaded = true,
-    path = "C:\\Users\\Nate\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-nvim-lsp",
-    url = "https://github.com/hrsh7th/cmp-nvim-lsp"
-  },
   ["gitsigns.nvim"] = {
     loaded = true,
     path = "C:\\Users\\Nate\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\gitsigns.nvim",
@@ -99,20 +89,15 @@ _G.packer_plugins = {
     path = "C:\\Users\\Nate\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\harpoon",
     url = "https://github.com/ThePrimeagen/harpoon"
   },
-  ["lsp-zero.nvim"] = {
+  ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\Nate\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\lsp-zero.nvim",
-    url = "https://github.com/VonHeikemen/lsp-zero.nvim"
+    path = "C:\\Users\\Nate\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\mason-lspconfig.nvim",
+    url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
-  ["nvim-cmp"] = {
+  ["mason.nvim"] = {
     loaded = true,
-    path = "C:\\Users\\Nate\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-cmp",
-    url = "https://github.com/hrsh7th/nvim-cmp"
-  },
-  ["nvim-lspconfig"] = {
-    loaded = true,
-    path = "C:\\Users\\Nate\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-lspconfig",
-    url = "https://github.com/neovim/nvim-lspconfig"
+    path = "C:\\Users\\Nate\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\mason.nvim",
+    url = "https://github.com/williamboman/mason.nvim"
   },
   ["nvim-treesitter"] = {
     loaded = true,
